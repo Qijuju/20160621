8633462acbe6b6b56767d8ccb94c2ae9e4b36c25
yourName <ming.you@hp.com>
2016-06-21 17:26:15 +0800

    这里写备注消息，如增减的功能，修改的内容等

---
+++ testDemo/testDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testDemo
+//
+//  Created by Oranqi on 6/21/16.
+//  Copyright © 2016 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testDemo/testDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testDemo
+//
+//  Created by Oranqi on 6/21/16.
+//  Copyright © 2016 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
